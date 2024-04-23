b8fd558e5b63297bf78bc0f857b25b047b6d6fd1
zee2-cyber <miyazanele69@gmail.com>
2024-04-23 13:22:09 +0200

    Add project files.

---
+++ week7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public interface IPlayable
+{
+    void Play();
+}
+
+public class Guitar : IPlayable
+{
+    public void Play()
+    {
+        Console.WriteLine("Guitar is now Playing");
+    }
+}
+
+public class Piano : IPlayable
+{
+    public void Play()
+    {
+        Console.WriteLine("Piano is now playing");
+    }
+
+    public static void Main(string[] args)
+    {
+        // Create instances of Guitar and Piano
+        var guitar = new Guitar();
+        var piano = new Piano();
+
+        // Play the instruments
+        guitar.Play();
+        piano.Play();
+
+        
+       
+    }
+}
